ecfc3b055986daf464e37ade6ee30b0887edd195
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2021-08-07 21:26:32 +0200

    refactoring into the MVC architecture

---
+++ src/js/model.js
@@ -0,0 +1,27 @@
+import { async } from 'regenerator-runtime/';
+import { API_URL } from './config';
+import { getJSOn } from './helper';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(state.recipe);
+  } catch (err) {
+    console.error(`${err} 😎😎😎`);
+  }
+};
