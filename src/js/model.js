eabb4f166f0b94a05c2dbf555a53169fe856a31a
Muhammed Mustafa <muhammed.mustafa.helal@gmail.com>
2021-08-08 14:54:28 +0200

    dynamically re-render DOM elements based on change in recipe view

--- src/js/model.js
+++ src/js/model.js
@@ -60,3 +60,14 @@ export const getSearchResultsPage = function (page = state.search.page) {
 
   return search.results.slice(start, end);
 };
+
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+
+    //newQt = oldQT * newservings / oldServings
+    ing.quantity = ing.quantity * newServings / state.recipe.servings;
+  });
+
+  state.recipe.servings = newServings;
+}
--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,14 +1,35 @@
 import icons from '../../img/icons.svg';
 
 export default class View {
-    _data;
+  _data;
   render(date) {
-    if(!data || (Array.isArray(data)) && data.length === 0) return this.renderError();
-    this.#data = data;
+    if (!data || (Array.isArray(data)) && data.length === 0) return this.renderError();
+    this._data = data;
     const markeup = this._generateMarkup();
-    recipeContainer.innerHTML = '';
     this.#clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  //DOM update dynamically (only update the changes to the recipe view not the whole component(view))
+  update(data) {
+    this._data = data;
+    const newMarkeup = this._generateMarkup();
+    const newDOM = document.createRange().createContextualFragment(newMarkeup);
+    const newElements = Array.from(newDOM.querySelectorALL('*'));
+    const curElements = Array.from(this._parentElement.querySelectorALL("*"));
+
+    //Update changed TEXT
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      if (newEl.isEqualNode(curEl) && newEl.firstChild.nodeValue.trim() !== '')
+        curEl.textContent = newEl.textContent;
+    });
+
+    //Update changed Attributes 
+    if (!newEl.isEqualNode(curEl))
+      Array.from(newEl.attributes).forEach(attr =>
+        curEl.setAttribute(attr.name, attr.value)
+      );
   }
 
   _clear() {
@@ -27,7 +48,7 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
-  renderMessage(message = this._errorMessage){
+  renderMessage(message = this._errorMessage) {
     const markup = `
     <div class="message">
       <div>
@@ -39,7 +60,7 @@ export default class View {
     </div>
     `;
     this._clear();
-    this._parentElement.insertAdjacentElement('afterbegin',  markup)
+    this._parentElement.insertAdjacentElement('afterbegin', markup)
   }
 
 }
